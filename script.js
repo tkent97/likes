553d7481c1cc27cc163cf3aa4161c372eac983d9
tkent97 <tristen123abc@gmail.com>
2021-07-01 00:00:08 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("page loading...");
+
+var likeCount1 = 9;
+var likeSpan1 = document.querySelector("#row-1");
+
+function likenum1() {
+    likeCount1++;
+    likeSpan1.innerText = likeCount1 + "like(s)";
+}
+
+var likeCount2 = 12;
+var likeSpan2 = document.querySelector("#row-2");
+
+function likenum2() {
+    likeCount2++;
+    likeSpan2.innerText = likeCount2 + "like(s)";
+}
+
+var likeCount3 = 9;
+var likeSpan3 = document.querySelector("#row-3");
+
+function likenum3() {
+    likeCount3++;
+    likeSpan3.innerText = likeCount3 + "like(s)";
+}
+
